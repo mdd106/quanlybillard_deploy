475d4bc560799be2801a27fd27c71996a67212a6
mdd106 <duongglead@gmail.com>
2026-06-11 16:03:39 +0700

    Fix db config: mysql to postgresql for render

--- BE/config/database.js
+++ BE/config/database.js
@@ -1,17 +1,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'billiards_db',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASS || '',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: false, // Set to console.log to see SQL queries
-    timezone: '+07:00' // Vietnam time
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  timezone: '+07:00'
+});
 
 const connectDB = async () => {
   try {
